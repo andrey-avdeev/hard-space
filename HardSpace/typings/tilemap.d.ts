8bd2c2a234f3970d128d603c507efe3f2b588ca3
Andrew Avdeev <seorazer@gmail.com>
2017-05-08 16:36:47 +0300

    intial

---
+++ HardSpace/typings/tilemap.d.ts
@@ -0,0 +1,93 @@
+﻿
+export interface ILevel {
+	width: number;
+	height: number;
+
+	tilewidth: number;
+	tileheight: number;
+
+	orientation: string;
+	renderorder: string;
+
+	nextobjectid: number;
+	version: number;
+
+	layers: ILayer[];
+	tilesets: ITileSet[];
+}
+
+export interface ILayer {
+	x: number;
+	y: number;
+	width: number;
+	height: number;
+
+	visible: boolean;
+	type: string;
+	opacity: number;
+	name: string;
+}
+
+export interface ITileLayer extends ILayer {
+	data: number[];
+}
+
+export interface IObjectLayer extends ILayer {
+	draworder: string;
+	objects: IGameObject[];
+}
+
+export interface IGameObject {
+	gid: number;
+	id: number;
+
+	x: number;
+	y: number;
+
+	width: number;
+	height: number;
+
+	rotation: number;
+
+	visible: boolean;
+	type: string;
+	name: string;
+
+	properties: any;
+	propertytypes: any;
+}
+
+export interface IPlayer extends IGameObject {
+
+}
+
+export interface IAsteroid extends IGameObject {
+
+}
+
+export interface IMessage extends IGameObject {
+
+}
+
+export interface IEvent extends IGameObject {
+
+}
+
+export interface ITileSet {
+	firstgid: number,
+
+	imagewidth: number,
+	imageheight: number,
+	image: string,
+
+	tilecount: number,
+	tilewidth: number,
+	tileheight: number,
+
+	name: string,
+
+	columns: number,
+
+	margin: number,
+	spacing: number,
+}
